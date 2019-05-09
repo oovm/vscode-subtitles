bd28e3520867f2f91414fcf995f2f27e2a7f06fa
Aster <galaster@foxmail.com>
2019-05-09 15:18:58 +0800

    🔧 Fix version info

---
+++ source/lib/index.ts
@@ -0,0 +1,7 @@
+import { readFileSync } from 'fs'
+import * as path from 'path'
+
+export function getVersion() {
+    const json = readFileSync(path.resolve(__dirname, '..', '..', 'package.json'))
+    return 'v' + JSON.parse(json.toString()).version
+}
