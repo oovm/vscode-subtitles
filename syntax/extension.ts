507402a9427f29f33f5cc272ca92878bf5b2d0ba
Aster <galaster@foxmail.com>
2019-05-09 15:21:16 +0800

    🌿 The Builder

---
+++ syntax/extension.ts
@@ -0,0 +1,21 @@
+import { writeFileSync } from 'fs'
+import { srt, lrc } from '../source'
+
+interface tmLang {
+    id: string
+    scopeName: string
+    uuid: string
+}
+
+
+
+function including(_: string) { return { include: '#' + _ } }
+function saveSyntax(name: tmLang) {
+    writeFileSync(
+        `${__dirname}/${name.id}.tmLanguage.json`,
+        JSON.stringify(name, null, 4),
+    )
+}
+
+[lrc, srt].map(saveSyntax)
+
