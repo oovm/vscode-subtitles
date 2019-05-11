0c82fc622eca21eaab7670f0d463e6415f063439
Aster <galaster@foxmail.com>
2019-05-12 00:59:38 +0800

    🔧 Fix ASS CommentLine

--- source/ass/main.ts
+++ source/ass/main.ts
@@ -1,3 +1,20 @@
+const digit = {
+    match: /(?<!\w)(\d*(\.\d*)?)(?!\w)/.source,
+    name: 'constant.numeric.ass',
+}
+
+const string = {
+    match: /(?<!\w)([^,]*)(?!\w)/.source,
+    name: 'string.literal.ass',
+}
+
+const illegal = {
+    match: '(.*)',
+    captures: {
+        1: { name: 'invalid.illegal.lyric' },
+    },
+}
+
 const invalid = [
     {
         name: 'invalid.illegal.noKey.ass',
@@ -6,42 +23,80 @@ const invalid = [
     {
         name: 'invalid.deprecated.noValue.ass',
         match: '(\\s*[A-Za-z_\\-][A-Za-z0-9_\\-]*\\s*:)(?:\\s*$)',
-    }
+    },
 ]
 
 
-const key_value = {
+const CommonLine = {
     begin: /([A-Za-z\s]*)(:)\s*/.source,
     beginCaptures: {
-        1: {
-            name: 'entity.name.tag.ass',
-        },
-        2: {
-            name: 'punctuation.definition.keyValue.ass',
-        },
+        1: { name: 'entity.name.tag.ass' },
+        2: { name: 'punctuation.definition.keyValue.ass' },
     },
-    end: /[^,]*\n/.source,
-    patterns: [
+    end: /\r?\n/.source,
+    patterns: [digit, string, illegal],
+}
 
+const CommentLine = {
+    match: /(Comment)(:)\s*(.*)\r?\n/.source,
+    captures: {
+        1: { name: 'punctuation.definition.comment.ass' },
+        2: { name: 'punctuation.definition.keyValue.ass' },
+        3: { name: 'comment.line.ass' },
+    },
+}
+
+const FormatLine = {
+    begin: /(Format)(:)/.source,
+    beginCaptures: {
+        1: { name: 'entity.name.tag.ass' },
+        2: { name: 'punctuation.definition.keyValue.ass' },
+    },
+    end: /[\r\n]+/.source,
+    //contentName: 'meta.function-call.arguments.python',
+    patterns: [
+        {
+            match: /,/.source,
+            name: 'punctuation.separator.ass',
+        },
+        {
+            match: /\b([a-zA-Z]\w*)/.source,
+            name: 'storage.type.ass'//'support.function.ass',
+        },
     ],
 }
 
+const StyleLine = {
+    begin: /(Style|Dialogue)(:)/.source,
+    beginCaptures: {
+        1: { name: 'entity.name.tag.ass' },
+        2: { name: 'punctuation.definition.keyValue.ass' },
+    },
+    end: /[\r\n]+/.source,
+    //contentName: 'meta.function-call.arguments.python',
+    patterns: [
+        {
+            match: /,/.source,
+            name: 'punctuation.separator.ass',
+        },
+        {
+            match: /\b([a-zA-Z]\w*)/.source,
+            name: 'storage.type.ass'//'support.function.ass',
+        },
+    ],
+}
 
 const comment = {
     begin: '(^[ \\t]+)?(?=;)',
     beginCaptures: {
-        1: {
-            name: 'punctuation.whitespace.comment.leading.ass',
-        },
+        1: { name: 'punctuation.whitespace.comment.leading.ass' },
     },
     end: '(?!\\G)',
     patterns: [
         {
             begin: ';',
             beginCaptures: {
-                0: {
-                    name: 'punctuation.definition.comment.ass',
-                },
+                0: { name: 'punctuation.definition.comment.ass' },
             },
             end: '\\n',
             name: 'comment.line.semicolon.ass',
@@ -52,7 +107,10 @@ const comment = {
 
 export const pattern: any = [
     comment,
-    key_value,
+    CommentLine,
+    FormatLine,
+    StyleLine,
+    CommonLine,
     {
         captures: {
             1: {
@@ -111,5 +169,5 @@ export const pattern: any = [
             },
         },
         name: 'string.quoted.double.ass',
-    }
+    },
 ]
